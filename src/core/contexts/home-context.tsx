dc59aa5c43050c889e12cb952ebced915fa5e7da
Iury <iurygfranca@gmail.com>
2023-01-13 17:46:26 -0400

    feat: first call to api ✨

--- src/core/contexts/home-context.tsx
+++ src/core/contexts/home-context.tsx
@@ -2,6 +2,19 @@ import { createContext, useContext, useState } from 'react';
 
 type HomeContextData = {
   postList: string[];
+  getPostsList: () => void;
+};
+
+export type ImagePost = {
+  src: string;
+  alt: string;
+  size: number[];
+};
+
+export type CardPostProps = {
+  image: ImagePost;
+  title: string;
+  slug: string;
 };
 
 export const HomeContext = createContext({} as HomeContextData);
@@ -9,10 +22,17 @@ export const HomeContext = createContext({} as HomeContextData);
 export function HomeProvider({ children }) {
   const [postList, setPostList] = useState([]);
 
+  function getPostsList() {
+    fetch('https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518')
+      .then(response => response.json())
+      .then(data => setPostList(data));
+  }
+
   return (
     <HomeContext.Provider
       value={{
         postList,
+        getPostsList,
       }}
     >
       {children}
--- src/core/views/Home/index.tsx
+++ src/core/views/Home/index.tsx
@@ -1,6 +1,16 @@
 import { PageTemplate } from '@/components/templates';
+import { useHomeContext } from '@/core/contexts';
+import { useEffect } from 'react';
 
 export const HomeView = () => {
+  const { postList, getPostsList } = useHomeContext();
+
+  useEffect(() => {
+    getPostsList();
+  }, []);
+
+  console.log(postList, 'postList');
+
   return (
     <PageTemplate title="Home">
       <p>Teste</p>
